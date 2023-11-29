556e6cf8368766133cb4392a84f368b45a1a2260
dumia16 <dujay77@gmail.com>
2023-11-28 20:26:50 -0800

    add users api

--- users/routes.js
+++ users/routes.js
@@ -6,23 +6,23 @@ function UserRoutes(app) {
     res.json(users);
   };
 
-  //   const findUserById = async (req, res) => {
-  //     const { userId } = req.params;
-  //     const user = await dao.findUserById(userId);
-  //     res.json(user);
-  //   };
+  const findUserById = async (req, res) => {
+    const { userId } = req.params;
+    const user = await dao.findUserById(userId);
+    res.json(user);
+  };
 
-  //   const updateUser = async (req, res) => {
-  //     const { userId } = req.params;
-  //     const status = await dao.updateUser(userId, req.body);
-  //     const currentUser = await dao.findUserById(userId);
-  //     req.session["currentUser"] = currentUser;
-  //     res.json(status);
-  //   };
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    const currentUser = await dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
+    res.json(status);
+  };
 
   app.get("/api/users", findAllUsers);
-  // app.get("/api/users/:userId", findUserById);
-  // app.put("/api/users/:userId", updateUser);
+  app.get("/api/users/:userId", findUserById);
+  app.put("/api/users/:userId", updateUser);
 }
 
 export default UserRoutes;
