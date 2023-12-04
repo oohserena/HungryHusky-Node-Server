9ff0d36e4c04585b91bf65738f389bd3ce59277b
Serena <serena@christophersmbp.lan>
2023-12-03 16:04:26 -0800

    add findFavoritesByUserId

--- favorites/dao.js
+++ favorites/dao.js
@@ -1,8 +1,15 @@
+import { removeFromUserFavorites } from "../users/dao.js";
 import favoriteModel from "./model.js";
 
 
 export const createFavorite = (favoriteData) => favoriteModel.create(favoriteData);
 export const deleteFavorite = (id) => favoriteModel.deleteOne({ _id: id });
-export const findFavoriteByUserId = (userId) => favoriteModel.find({
-    user_id: userId
-});
+export const findFavoriteByUserId = (userId) => {
+    return favoriteModel.find({ user_id: userId })
+        .then(favorites => favorites)
+        .catch(error => {
+            console.error('Error finding reviews by user ID:', error);
+            throw error; 
+        });
+};
+
