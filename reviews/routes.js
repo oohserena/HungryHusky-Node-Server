acf8426c5454c9b287d61dd402285bcb6fb0db14
dumia16 <dujay77@gmail.com>
2023-12-05 00:17:11 -0800

    remove duplicate deleteReviewById

--- reviews/routes.js
+++ reviews/routes.js
@@ -3,35 +3,19 @@ import * as userDao from "../users/dao.js";
 import mongoose from "mongoose";
 
 function ReviewRoutes(app) {
-    const createReview = async (req, res) => {
-        try {
-            const newReview = await reviewDao.createReview({
-                user_id: req.body.user_id,
-                restaurant_id: req.body.restaurant_id,
-                content: req.body.content,
-            });
-            await userDao.addToUserReviews(req.body.user_id, newReview._id);
-            res.status(201).json(newReview);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
-    };
-
-    const deleteReviewById = async (req, res) => {
-        try {
-            const { id } = req.params;
-            // console.log(id)
-            const deletedReview = await reviewDao.findReviewById(id);
-            // console.log(deletedReview)
-            if (!deletedReview) {
-                return res.status(404).json({
-                    message: "Review not found"
-                });
-            }
-            // console.log(deletedReview)
-            // console.log(id)
-            // console.log(deletedReview.user_id)
-
+  const createReview = async (req, res) => {
+    try {
+      const newReview = await reviewDao.createReview({
+        user_id: req.body.user_id,
+        restaurant_id: req.body.restaurant_id,
+        content: req.body.content,
+      });
+      await userDao.addToUserReviews(req.body.user_id, newReview._id);
+      res.status(201).json(newReview);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
 
   const deleteReviewById = async (req, res) => {
     try {
@@ -56,45 +40,42 @@ function ReviewRoutes(app) {
       });
     }
   };
-          
+
   const findAllReviews = async (req, res) => {
-        const reviews = await reviewDao.findAllReviews();
-        res.json(reviews);
-   };
+    const reviews = await reviewDao.findAllReviews();
+    res.json(reviews);
+  };
 
   const findReviewsByUserId = async (req, res) => {
-      try {
-          const userId = req.params.userId;
-          console.log(userId)
-          const reviews = await reviewDao.findReviewsByUserId(userId);
-          res.json(reviews);
-          console.log(reviews)
-      } catch (error) {
-          console.error(error);
-          res.status(500).send({message: error.message});
-      }
+    try {
+      const userId = req.params.userId;
+      console.log(userId);
+      const reviews = await reviewDao.findReviewsByUserId(userId);
+      res.json(reviews);
+      console.log(reviews);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: error.message });
+    }
   };
 
   const findReviewsByRestaurantId = async (req, res) => {
-      try {
-          const restaurantId = req.params.restaurantId;
-          const reviews = await reviewDao.findReviewsByRestaurantId(restaurantId);
-          res.json(reviews);
-          console.log(reviews)
-      } catch (error) {
-          console.error(error);
-          res.status(500).send({message: error.message});
-      }
+    try {
+      const restaurantId = req.params.restaurantId;
+      const reviews = await reviewDao.findReviewsByRestaurantId(restaurantId);
+      res.json(reviews);
+      console.log(reviews);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: error.message });
     }
+  };
 
-
-    app.post("/api/reviews", createReview);
-    app.delete("/api/reviews/:id", deleteReviewById);
-    app.get("/api/reviews", findAllReviews);
-    app.get("/api/users/:userId/review", findReviewsByUserId);
-    app.get("/api/restaurants/:restaurantId/review", findReviewsByRestaurantId);
-
-
+  app.post("/api/reviews", createReview);
+  app.delete("/api/reviews/:id", deleteReviewById);
+  app.get("/api/reviews", findAllReviews);
+  app.get("/api/users/:userId/review", findReviewsByUserId);
+  app.get("/api/restaurants/:restaurantId/review", findReviewsByRestaurantId);
 }
 
 export default ReviewRoutes;
