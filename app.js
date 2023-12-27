1c7eccc827cd4618358de2118b67d09441225527
Serena <serena@christophersmbp.lan>
2023-12-26 20:53:46 -0800

    12-26-push

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    // origin: process.env.FRONTEND_URL,
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 
